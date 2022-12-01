80a268c028ebbfc2b67e3d3d43728dac6d49b0f0
Hakan <hakantekir2002@outlook.com>
2022-12-01 11:30:57 +0300

    Initial Commit

---
+++ iCoffee/iCoffeeApp.swift
@@ -0,0 +1,20 @@
+//
+//  iCoffeeApp.swift
+//  iCoffee
+//
+//  Created by Hakan Tekir on 1.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct iCoffeeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
